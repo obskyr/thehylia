fc9800296758bfa188c0fb83042e57ecbab4c3bf
obskyr <powpowd@gmail.com>
2018-06-26 22:07:51 +0200

    Minor style fix.
    Also, removed a piece of testing code.

--- thehylia.py
+++ thehylia.py
@@ -185,13 +185,13 @@ def friendlyDownloadFile(file, path, index, total, verbose=False):
     filename = original_filename.encode(encoding, 'replace').decode(encoding)
     path = os.path.join(path, filename)
 
-    by_the_way = ""
-    if original_filename != filename or True:
-        by_the_way = " (replaced characters not in the filesystem's \"{}\" encoding)".format(encoding)
+    byTheWay = ""
+    if original_filename != filename:
+        byTheWay = " (replaced characters not in the filesystem's \"{}\" encoding)".format(encoding)
     
     if not os.path.exists(path):
         if verbose:
-            unicodePrint("Downloading {}: {}{}...".format(numberStr, filename, by_the_way))
+            unicodePrint("Downloading {}: {}{}...".format(numberStr, filename, byTheWay))
         for triesElapsed in range(3):
             if verbose and triesElapsed:
                 unicodePrint("Couldn't download {}. Trying again...".format(filename), file=sys.stderr)
@@ -207,7 +207,7 @@ def friendlyDownloadFile(file, path, index, total, verbose=False):
             return False
     else:
         if verbose:
-            unicodePrint("Skipping over {}: {}{}. Already exists.".format(numberStr, filename, by_the_way))
+            unicodePrint("Skipping over {}: {}{}. Already exists.".format(numberStr, filename, byTheWay))
 
     return True
 
